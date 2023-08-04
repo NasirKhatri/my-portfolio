a4ae2df1fa663f6f90c4a6dec04690b1737fb34d
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2023-08-04 20:35:26 +0500

    contact button link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
               <Typography variant="h3">Full-Stack Developer</Typography>
               <Typography variant="body1" sx={{ marginTop: "25px", marginBottom: "25px", textAlign: "justify" }}>I am a MERN Stack Developer, I work with ReactJS, React Native, NextJS, NodeJS, ExpressJS, MongoDB etc to develop web and mobile applications. Also carrying experience to work with third party libraries like MUI, Formik, Yup, Mongoose and many more. </Typography>
               <Button variant="outlined" startIcon={<DownloadIcon />} sx={{ marginRight: "10px" }}>Resume</Button>
-              <Button variant="contained" startIcon={<CallIcon />}>Contact</Button>
+              <Button variant="contained" startIcon={<CallIcon />} href="/contact">Contact</Button>
             </Box>
           </Grid>
           <Grid item xs={12} md={6} textAlign="center">
